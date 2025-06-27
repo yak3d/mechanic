7d274a30d0b82c95a7eeeacb973dbacd61c19704
yak <yak@yak3d.com>
2025-06-27 01:06:11 -0400

    add missing log messages

---
+++ Mechanic.CLI/Infrastructure/Logging/LogMessages.cs
@@ -0,0 +1,10 @@
+﻿using Mechanic.CLI.Models;
+using Microsoft.Extensions.Logging;
+
+namespace Mechanic.CLI.Infrastructure.Logging;
+
+public static partial class LogMessages
+{
+    [LoggerMessage(Level = LogLevel.Information, Message = "Assuming game file type is {GameFileType} based on the extension {Extension}")]
+    public static partial void AssumingGameFileType(this ILogger logger, GameFileType gameFileType, string extension);
+}
